44c069b954ad9da6725edc2493cb4092b8dc2e08
Ganesh Mahadik <gmahadik93@gmail.com>
2020-05-30 02:10:11 +0530

    Updated admin to support custom user model

--- grocery/cart/tests/test_admin.py
+++ grocery/cart/tests/test_admin.py
@@ -1,27 +1,41 @@
-# from django.test import TestCase, Client
-# from django.contrib.auth import get_user_model
-# from django.urls import reverse
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
-# class AdminSiteTests(TestCase):
+class AdminSiteTests(TestCase):
 
-#     def setUp(self):
-#         self.client = Client()
-#         self.admin_user = get_user_model().objects.create_superuser(
-#             email='admin@ganam.com',
-#             password='admin@123'
-#         )
-#         self.client.force_login(self.admin_user)
-#         self.user = get_user_model.objects.create_user(
-#             email='test@ganam.com',
-#             password='user@123',
-#             name='Test user  full name'
-#         )
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@ganam.com',
+            password='admin@123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@ganam.com',
+            password='user@123',
+            name='Test user full name'
+        )
 
-#     def test_users_listed(self):
-#         """Test that users are listed on user page"""
-#         url = reverse('admin:cart_user_changelist')
-#         res = self.client.get_user_model(url)
-        
-#         self.assertContains(res, self.user.name)
-#         self.assertContains(res, self.user.email)
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:cart_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that user edit page works"""
+        url = reverse('admin:cart_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the Create user page is work"""
+        url = reverse('admin:cart_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
