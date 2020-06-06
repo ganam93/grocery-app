26bc352e1c26d01ed2f4bf80b6f490b1528103d1
Ganesh Mahadik <gmahadik93@gmail.com>
2020-06-07 00:08:54 +0530

    Added manage user endpoint

--- grocery/user/serializers.py
+++ grocery/user/serializers.py
@@ -16,6 +16,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validates_data):
+        """Update a user, setting a password correctly and return it"""
+        password = validates_data.pop('password', None)
+        user = super().update(instance, validates_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serislizer for user authentication object"""
